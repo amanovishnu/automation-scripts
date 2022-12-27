5829d12bdc12b8a7dcb9fa8fa7921aba8f28e36e
Mano Vishnu <geekymano@gmail.com>
2022-12-27 10:58:18 +0530

    fix: anaconda setup script updated

--- anaconda_installation.sh
+++ anaconda_installation.sh
@@ -1,24 +1,29 @@
+# Reference: https://docs.anaconda.com/anaconda/install/linux/ (Official Guide)
+# Reference : https://linuxize.com/post/how-to-install-anaconda-on-ubuntu-20-04/
+
+# Author: Mano Vishnu (geekymano@gmail.com)
+# Last Updated: 2022/12/27
+
+# command to execute: bash anaconda_installation.sh <path_to_execute_setup_file>
+
 echo '-------------------------------------- Prerequisites for Installing Anaconda --------------------------------------'
-sudo apt update ;
+sudo apt update -y;
 sudo apt full-upgrade -y;
-sudo apt autoremove;
-sudo apt clean;
+sudo apt autoremove -y;
+sudo apt clean -y;
 
 echo '----------------------- Installing Anaconda Navigator Dependencies -----------------------------'
 sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
 echo '----------------------- Downloading Anaconda Setup file -----------------------------'
-wget -P <path_to_save_file> https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+wget -P $1 https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 
 echo '----------------------- Verifying Anaconda Setup file -----------------------------'
-sha256sum <path_to_saved_file>/Anaconda3-2020.02-Linux-x86_64.sh
+sha256sum $1/Anaconda3-2020.02-Linux-x86_64.sh
 
 echo '----------------------- Executing Anaconda Setup file -----------------------------'
-bash <path_to_saved_file>/Anaconda3-2020.02-Linux-x86_64.sh
+bash $1/Anaconda3-2020.02-Linux-x86_64.sh
 
+echo '----------------------- Updating bashrc file & Starting anaconda-navigator'
 source ~/.bashrc
 anaconda-navigator
-
-# Replace <path_to_saved_file> with . to download the file to current directory (pwd)
-# Reference: https://docs.anaconda.com/anaconda/install/linux/ (Official Guide)
-# Reference : https://linuxize.com/post/how-to-install-anaconda-on-ubuntu-20-04/
